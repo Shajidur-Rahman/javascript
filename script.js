b392539a100f567febb7a0d672bb98fd410b509a
Shajidur-Rahman <shajudurrahmansaad@gmail.com>
2021-04-19 16:59:27 +0600

    tutorial 51 finished

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 let name = "shajidur rahman"; // ';' is not case sensetive 
 // printing to the browser console
 
-console.log(name); 
+console.log(name);
 
 console.log(`my name is ${name}`); // `` is must needed
 
@@ -26,13 +26,13 @@ console.log(`the position of 's' is ${position}`);
 
 
 // substring strings 
-let subs = js.slice(1,3); // can handle negetive value and will count like this (1,3) = {1,(3-1)} = (1,2)
+let subs = js.slice(1, 3); // can handle negetive value and will count like this (1,3) = {1,(3-1)} = (1,2)
 console.log(subs)
 
-let substring = js.substring(1,3); // can't handle negetive value and will count like this (1,3) = {1,(3-1)} = (1,2)
+let substring = js.substring(1, 3); // can't handle negetive value and will count like this (1,3) = {1,(3-1)} = (1,2)
 console.log(substring);
 
-let subsr = js.substr(1,3); // 
+let subsr = js.substr(1, 3); // 
 console.log(subsr);
 
 // replaceing items 
@@ -47,3 +47,50 @@ console.log(js.toUpperCase());  // the () is must
 // lowercase
 
 console.log(js.toLowerCase());
+
+// tutorial 50 finished
+// tutorial 51 started 
+
+// scope 
+const a = "it cant be changed "  // const cant be changed 
+
+// if else conditions
+
+let age = 2;
+if(age > 18){
+    console.log('you cant drink it ');
+}
+else if (age == 12){
+    console.log('you are 12');
+}
+else {
+    console.log('you can drink it ');
+}
+
+// switch 
+
+let cups = 20;
+switch (cups) {  // break must needed
+    case 23:
+        console.log('there are 23 cups')
+        break;
+    case 74:
+        console.log('there are 74 cups')
+        break;
+    case 221:
+        console.log('there are 221 cups')
+        break;
+    case 2:
+        console.log('there are 2 cups')
+        break;
+    case 203:
+        console.log('there are 20 cups')
+        break;
+    case 2323:
+        console.log('there are 233434 cups')
+        break;
+
+    default:
+        console.log('the value of cups is none if them')
+        break;
+}
